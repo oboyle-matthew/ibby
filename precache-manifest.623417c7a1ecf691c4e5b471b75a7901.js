6f92af02541a5ca3abb4f34634eed9114df0efab
Feroze Mohideen <fm87@duke.edu>
2020-01-29 05:22:43 -0500

    Updates

---
+++ precache-manifest.623417c7a1ecf691c4e5b471b75a7901.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ccd3eeb1a016e3fa0a95057a0cc936",
+    "url": "/ibby/index.html"
+  },
+  {
+    "revision": "741c17241afd949162b4",
+    "url": "/ibby/static/css/main.d10c1ce9.chunk.css"
+  },
+  {
+    "revision": "049b3eb24ab6335a857c",
+    "url": "/ibby/static/js/2.f3977867.chunk.js"
+  },
+  {
+    "revision": "3a5da4106a08e596432e5e2628e1ef8d",
+    "url": "/ibby/static/js/2.f3977867.chunk.js.LICENSE"
+  },
+  {
+    "revision": "741c17241afd949162b4",
+    "url": "/ibby/static/js/main.64e6c552.chunk.js"
+  },
+  {
+    "revision": "9414c0cfe892313300ec",
+    "url": "/ibby/static/js/runtime-main.0809da06.js"
+  },
+  {
+    "revision": "e0c7d2c7767a026c9d7d6ef7ebbcf09b",
+    "url": "/ibby/static/media/BestBuddies.e0c7d2c7.jpg"
+  },
+  {
+    "revision": "46fa7c0847f050f0f4a613a0a182d10d",
+    "url": "/ibby/static/media/Map.46fa7c08.svg"
+  },
+  {
+    "revision": "674217ab375b702480654f2bd4fabd65",
+    "url": "/ibby/static/media/connect_ibby.674217ab.svg"
+  },
+  {
+    "revision": "f0cbf90c53f0d4dbe7f566d6cabe0383",
+    "url": "/ibby/static/media/fb.f0cbf90c.svg"
+  },
+  {
+    "revision": "c7124afd957ffb20fa6ccf3fb23d3a78",
+    "url": "/ibby/static/media/friendsofibbybg.c7124afd.svg"
+  },
+  {
+    "revision": "935f75192b616d9710e9564748b9faf9",
+    "url": "/ibby/static/media/ibby-export.935f7519.png"
+  },
+  {
+    "revision": "2bb7f7693a511155bd4db012b319c8db",
+    "url": "/ibby/static/media/ibby_chin.2bb7f769.jpg"
+  },
+  {
+    "revision": "124252925901b9fd815f860678913c7d",
+    "url": "/ibby/static/media/ibby_cutout.12425292.png"
+  },
+  {
+    "revision": "cd9df573bcc304f2e7998f7dc1a50adf",
+    "url": "/ibby/static/media/ibbyatdukebackground.cd9df573.svg"
+  },
+  {
+    "revision": "6e6648df2646b42d89ae670c02f973d4",
+    "url": "/ibby/static/media/insta.6e6648df.svg"
+  },
+  {
+    "revision": "d41d11f60eac30bbf79b2d502d1fee53",
+    "url": "/ibby/static/media/join.d41d11f6.svg"
+  },
+  {
+    "revision": "1468a23c0fdbc471a85cbeddcc885873",
+    "url": "/ibby/static/media/left-arrow.1468a23c.svg"
+  },
+  {
+    "revision": "04103587ee06cde8d44f564b75ca9f85",
+    "url": "/ibby/static/media/org.04103587.png"
+  },
+  {
+    "revision": "375e1a6a1d14fd29a519d71affb313fb",
+    "url": "/ibby/static/media/pushpin.375e1a6a.png"
+  },
+  {
+    "revision": "ab01a5d655bb9dea81159e2c849f5bbd",
+    "url": "/ibby/static/media/signature.ab01a5d6.svg"
+  },
+  {
+    "revision": "7f9506989fa78b24ccc942868c206783",
+    "url": "/ibby/static/media/splash_ibby.7f950698.jpg"
+  },
+  {
+    "revision": "196a7dae39a8d0cb0346b58ecaf6f016",
+    "url": "/ibby/static/media/why-yt-graphic.196a7dae.svg"
+  }
+]);
